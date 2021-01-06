af6d819e6c6097c02fe9d184650d3455cdaafd54
owenmoogk <owenmoogk@gmail.com>
2021-01-06 17:14:25 -0500

    cleaned up some code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import smtplib
 import credentials
 
-sender, password = credentials.getCredentials()
-
 def get_contacts(filename):
     names = []
     emails = []
@@ -18,18 +16,22 @@ def read_template(filename):
     template_file_content = template_file.read()
     return (template_file_content)
 
-names, emails = get_contacts('mycontacts.txt') # read contacts
-message_template = read_template('message.txt') # read the message template
-
-for i in range(0,len(names)):
-    name = names[i]
-    email = emails[i]
-
-    message = message_template.format(PERSON_NAME = name, EMAIL = email)
-
+def sendEmail(name, email):
+    message = message_template.format(PERSON_NAME = name)
     s = smtplib.SMTP_SSL('smtp.gmail.com')
     s.login(sender, password)
     s.sendmail(sender, email, message)
     s.quit()
 
-print("Sent!")
+sender, password = credentials.getCredentials() # get my email credentials
+message_template = read_template('message.txt') # read the message template
+names, emails = get_contacts('mycontacts.txt') # read contacts
+
+# loop thru all
+for i in range(0,len(names)):
+    name = names[i]
+    email = emails[i]
+    sendEmail(name, email)
+    print("Sent")
+
+print("Finished")
