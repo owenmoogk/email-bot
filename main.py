23418b4879eda608753204c254a9a064eb5c3934
owenmoogk <owenmoogk@gmail.com>
2021-02-17 09:30:11 -0500

    added the wrdsb screening thing (for fun)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import smtplib
-import credentials
+import smtplib, credentials, datetime
 
 def get_contacts(filename):
     names = []
@@ -17,11 +16,21 @@ def read_template(filename):
     return (template_file_content)
 
 def sendEmail():
-    message = message_template.format(PERSON_NAME = name)
+    date = getDate()
+    message = message_template.format(DATE = date)
     s = smtplib.SMTP_SSL('smtp.gmail.com')
     s.login(sender, password)
     s.sendmail(sender, email, message)
     s.quit()
+
+def getDate():
+    date = datetime.datetime.now()
+    weekday = date.strftime("%A")
+    month = date.strftime("%B")
+    day = date.strftime("%d")
+    year = date.strftime("%Y")
+    return(weekday+", "+month+" "+day+", "+year+" 7:43:39 AM")
+
 
 sender, password = credentials.getCredentials() # get my email credentials
 message_template = read_template('message.txt') # read the message template
@@ -29,7 +38,7 @@ names, emails = get_contacts('mycontacts.txt') # read contacts
 
 # loop thru all
 for i in range(0,len(names)):
-    name = names[i]
+    # name = names[i]
     email = emails[i]
     sendEmail()
     print("Sent")
