4f2f37ff128daa3d0128e8897287e6f8a94ee969
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-12-21 22:08:28 -0500

    updated urls with regex

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import index
 
 urlpatterns = [
-    path('', index),
-    path('login/', index),
-    path('signup/', index),
-    path('compose/', index),
-    path('contacts/', index),
-    path('templates/', index)
+    re_path(r'^.*/$', index),
+    path('', index)
 ]
