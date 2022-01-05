f034c84024ab5d7ec088539eb01d8b09e232f0d9
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-01-04 19:40:27 -0500

    nav css

--- src/frontend/src/components/Nav.js
+++ src/frontend/src/components/Nav.js
@@ -2,35 +2,34 @@ import React from 'react';
 
 export default function Nav(props) {
 
-    function loggedInNav() {
-        return (
-            <>
-                <a href='/'>Home</a>
-                <a href='/compose'>Compose</a>
-                <a href='/contacts'>Contacts</a>
-                <a href='/templates'>Templates</a>
-                <button onClick={props.handleLogout}>Logout</button>
-                <a>{props.username}</a>
-            </>
-        )
-    }
+	function loggedInNav() {
+		return (
+			<>
+				<a href='/'>Home</a>
+				<a href='/compose'>Compose</a>
+				<a href='/contacts'>Contacts</a>
+				<a href='/templates'>Templates</a>
+				<a onClick={props.handleLogout}>Logout ({props.username})</a>
+			</>
+		)
+	}
 
-    function loggedOutNav() {
-        return (
-            <>
-                <a href='/'>Home</a>
-                <a href='/login'>Login</a>
-                <a href='/signup'>Signup</a>
-            </>
-        )
-    }
+	function loggedOutNav() {
+		return (
+			<>
+				<a href='/'>Home</a>
+				<a href='/login'>Login</a>
+				<a href='/signup'>Signup</a>
+			</>
+		)
+	}
 
-    return (
-        <div id='nav'>
-            {props.loggedIn
-                ? loggedInNav()
-                : loggedOutNav()
-            }
-        </div>
-    )
+	return (
+		<div id='nav'>
+			{props.loggedIn
+				? loggedInNav()
+				: loggedOutNav()
+			}
+		</div>
+	)
 }
